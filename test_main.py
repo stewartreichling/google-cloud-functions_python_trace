e19812f7ac85d6d17c549878f74171c02bb6e4e1
ymcmstew <stewart.reichling@gmail.com>
2018-08-20 21:16:58 -0700

    Generate X-Cloud-Trace-Context that looks like GCP's header for local testing

--- test_main.py
+++ test_main.py
@@ -6,11 +6,21 @@ import random
 import string
 
 
+def generate_trace_context():
+    """Generate an ID that looks like Google's X-Cloud-Trace-Context"""
+    charset = string.hexdigits.lower()
+    trace_id = ''.join(random.choices(charset, k=32))
+    span_id = ''.join(random.choices(charset, k=16))
+    options = 'o=0'
+    x_cloud_trace_context = f'{trace_id}/{span_id};{options}'
+    return x_cloud_trace_context
+
+
 def test():
     """Invoke the function's entrypoint, passing a trace_id via headers."""
     app = flask.Flask(__name__)
-    trace_id = f"dev_{''.join(random.choices(string.ascii_letters, k=8))}"
-    headers = {'X-Cloud-Trace-Context': trace_id}
+    x_cloud_trace_context = generate_trace_context()
+    headers = {'X-Cloud-Trace-Context': x_cloud_trace_context}
     with app.test_request_context(headers=headers):
         print(entrypoint(flask.request))
 
